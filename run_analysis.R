aed7eaf155e37d6ca5e04d602e0b39231b56e77b
rwfoxjr <roxr@bellsouth.net>
2016-01-31 11:19:57 -0600

    SundayMorning
    GitHub project setup

--- run_analysis.R
+++ run_analysis.R
@@ -1,11 +1,17 @@
-#Merge the training and the test sets to create one data set.
-#open the three files
-xtestFile <- read.delim("~/Documents/Coursera/wk4Project/UCI HAR Dataset/test/X_test.txt", header = FALSE, sep = "\t", stringsAsFactors = FALSE)
-ytestFile <- read.delim("~/Documents/Coursera/wk4Project/UCI HAR Dataset/test/y_test.txt", header = FALSE, sep = "\t")
-subjectTestFile <- read.delim("~/Documents/Coursera/wk4Project/UCI HAR Dataset/test/subject_test.txt", header = FALSE, sep = "\t")
-xtrainFile <- read.delim("~/Documents/Coursera/wk4Project/UCI HAR Dataset/train/X_train.txt", header = FALSE, sep = "\t", stringsAsFactors = FALSE)
-ytrainFile <- read.delim("~/Documents/Coursera/wk4Project/UCI HAR Dataset/train/y_train.txt", header = FALSE, sep = "\t")
-subjectTrainFile <- read.delim("~/Documents/Coursera/wk4Project/UCI HAR Dataset/train/subject_train.txt", header = FALSE, sep = "\t")
+#download UCI HAR dataset
+urlUcihar <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
+if(!file.exists("./ucihar")) {dir.create("./ucihar")}
+download.file(urlUcihar, destfile = "./ucihar/dataset.zip", method = "curl")
+dataZip <- "./ucihar/dataset.zip"
+unzip(dataZip,exdir = "./ucihar")
+
+#open the three files each for test and train 
+xtestFile <- read.delim("./ucihar/UCI HAR Dataset/test/X_test.txt", header = FALSE, sep = "\t", stringsAsFactors = FALSE)
+ytestFile <- read.delim("./ucihar/UCI HAR Dataset/test/y_test.txt", header = FALSE, sep = "\t")
+subjectTestFile <- read.delim("./ucihar/UCI HAR Dataset/test/subject_test.txt", header = FALSE, sep = "\t")
+xtrainFile <- read.delim("./ucihar/UCI HAR Dataset/train/X_train.txt", header = FALSE, sep = "\t", stringsAsFactors = FALSE)
+ytrainFile <- read.delim("./ucihar/UCI HAR Dataset/train/y_train.txt", header = FALSE, sep = "\t")
+subjectTrainFile <- read.delim("./ucihar/UCI HAR Dataset/train/subject_train.txt", header = FALSE, sep = "\t")
 
 #unpack the numeric data fom lists within lists
 unpackList <- function(x) {
@@ -19,7 +25,6 @@ unpackList <- function(x) {
     meanTest[i] <- mean(vectorTest,na.rm = TRUE)
     sdTest[i] <- sd(vectorTest,na.rm = TRUE)
   }
-  #Extract only the measurements on the mean and standard deviation for each measurement.
   resultTest <- data.frame(meanTest,sdTest)
   return(resultTest)
 }
